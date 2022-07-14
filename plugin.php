a7a406cfb35149eb65160e1049bf5466f2323a82
Mitchell Blair Chelin <mitchell.b.chelin@gmail.com>
2022-07-14 13:01:13 +1000

    Update Plugin License
    Update Plugin License from GPL2+ to MIT, Update Author URI @Link to Github

--- plugin.php
+++ plugin.php
@@ -6,19 +6,19 @@
  * This plugin is based on the Docsify project.
  * https://docsify.js.org/
  *
- * @link		https://mitchell-chelin.com/project/wp-docsify/
+ * @link		https://github.com/mitchell-b-chelin/WPDocsify/blob/main/LICENSE
  * @since 		1.0.1
  * @package		MBC
  *
  * @wordpress-plugin
  * Plugin Name:	   	WordPress Docsify
- * Plugin URI:		https://mitchell-chelin.com/project/wp-docsify/
+ * Plugin URI:		https://github.com/mitchell-b-chelin/WPDocsify/blob/main/LICENSE
  * Description:	   	A simple and lightweight plugin to create a Wordpress Documentation.
  * Version:		   	1.0.1
  * Author:			Mitchell-Blair Chelin
- * Author URI:		https://mitchell-chelin.com/
- * License:		   	GPL-2.0+
- * License URI:	   	http://www.gnu.org/licenses/gpl-2.0.txt
+ * Author URI:		https://github.com/mitchell-b-chelin/
+ * License:		   	MIT
+ * License URI:	   	https://github.com/mitchell-b-chelin/WPDocsify/blob/main/LICENSE
  */
 
 namespace MBC;
